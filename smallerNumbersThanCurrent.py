052cf069202eeebd42001ca9537507d89a0e001a
Milkesa Tesema <milkesatesemawakwaya@gmail.com>
2026-03-29 10:33:55 +0300

    smallerNumbersThanCurrent-Leetcode

---
+++ smallerNumbersThanCurrent.py
@@ -0,0 +1,11 @@
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        r=[]
+        for i in range(n):
+            count = 0
+            for j in range(n):
+                if nums[j] < nums[i]:
+                    count += 1
+            r.append(count)
+        return r
